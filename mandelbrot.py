80b650416100816f84c3bef345c7cc1f9d0c5db5
Lotte Heek <lotteheek@gmail.com>
2019-10-30 10:02:00 +0100

    structure

---
+++ mandelbrot.py
@@ -0,0 +1,9 @@
+"""
+Stochastic Simulation Assignment 1 - Mandelbrot
+
+"""
+
+# 1 - Study the problem, implement the iteration, create colorful pictures of the fractal
+# 2 - Investigate the convergence of Ais > Am
+# 3 - Compare results using pure random sampling, latin hypercube sampling & orthogonal sampling
+# 4 - Formulate & test a method to further improve the convergence rate of the Monte Carlo approach
